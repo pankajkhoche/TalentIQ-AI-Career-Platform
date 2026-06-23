7e60d6643d8dad9f0546b5667f93132f51dc5f67
Pankaj Khoche <khochepankaj@gmail.com>
2026-06-23 23:50:29 +0530

    Fix Render databse config

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise Exception("DATABASE_URL environment variable is not set")
+    raise Exception("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 
